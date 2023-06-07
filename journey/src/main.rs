4633f024a8900844bffb5b4829ebd4df1ed5e6ba
Benjamin Larkey <60051088+larkben@users.noreply.github.com>
2023-06-06 22:54:15 -0500

    Rust Terminal UI; Implementation.

---
+++ journey/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    for i in 0..1000000 {
+        let hello: String = "hello".to_string();
+        print!("\x1B[2J\x1B[1;1H"); // clearing the terminal screen
+        println!("{}", i);
+    }
+}
