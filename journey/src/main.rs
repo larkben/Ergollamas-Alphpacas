f913059b1f23527e4193f5525247bea01943d9de
Benjamin Larkey <60051088+larkben@users.noreply.github.com>
2023-06-10 15:46:22 -0500

    second api call

--- journey/src/main.rs
+++ journey/src/main.rs
@@ -2,8 +2,14 @@
 
 // https://api.ergoplatform.com/api/v1/addresses/9eo3TX7MXiNn9xbHTrgK21dEbQbSDpJNj9ybo3deoubzYVHkjD8/balance/confirmed
 
+#[cfg(target_arch = "x86_64")]
+#[cfg(feature = "stdsimd")]
+use std::arch::x86_64::_mm_mask_ror_epi32;
+
 use reqwest::{self, Response, Error};
 use serde::{Deserialize, Serialize};
+
+// First API CALL
 
 // Deserialization
 #[derive(Serialize, Deserialize, Debug)]
@@ -24,16 +30,86 @@ struct TokenOut {
     token_type: String,
 }
 
+// SECOND API CALL
+#[derive(Debug, serde::Deserialize)]
+struct Asset {
+    #[serde(rename = "tokenId")]
+    token_id: String,
+    index: u32,
+    amount: u32,
+    name: String,
+    decimals: u32,
+    #[serde(rename = "type")]
+    token_type: String,
+}
+
+#[derive(Debug, serde::Deserialize)]
+struct AdditionalRegisters {
+    #[serde(rename = "R5")]
+    r_5: String,
+    #[serde(rename = "R6")]
+    r_6: String,
+    #[serde(rename = "R8")]
+    r_8: String,
+    #[serde(rename = "R7")]
+    r_7: String,
+    #[serde(rename = "R9")]
+    r_9: String,
+    #[serde(rename = "R4")]
+    r_4: String,
+}
+
+#[derive(Debug, serde::Deserialize)]
+struct Item {
+    id: String,
+    #[serde(rename = "txId")]
+    tx_id: String,
+    value: u32,
+    index: u32,
+    #[serde(rename = "creationHeight")]
+    creation_height: u32,
+    #[serde(rename = "ergoTree")]
+    ergo_tree: String,
+    address: String,
+    assets: Vec<Asset>,
+    #[serde(rename = "additionalRegisters")]
+    additional_registers: AdditionalRegisters,
+    #[serde(rename = "spentTransactionId")]
+    spent_transaction_id: String,
+    #[serde(rename = "mainChain")]
+    main_chain: bool,
+}
+
 // Fetches the Token
 async fn get_token(address: String) -> Result<(), reqwest::Error> {
-    let api_call = format!(
+    let test_token_id: String = "8cf4295cf2eb3a7b6c2a44fc00483fb7101c1818b36f0b2fc807fe082e5f9853".to_string();
+
+    let wallet_call = format!(
         "https://api.ergoplatform.com/api/v1/addresses/{}/balance/confirmed",
         address
     );
 
-    let response = reqwest::get(&api_call).await?.json::<WalletOut>().await?;
+    let response = reqwest::get(&wallet_call).await?.json::<WalletOut>().await?; // Notice the syntax here
 
-    println!("{:#?}", response);
+    for token in response.tokens {
+        let token_id = token.token_id;
+
+        let token_issue: String = format!(
+            "https://api.ergoplatform.com/api/v0/assets/{}/issuingBox", 
+            token_id
+        );
+
+        //let issued_response: Item = reqwest::get(&token_issue).await?.json::<Item>().await?;
+
+        /*
+        if issued_response.address == "9i27sKZ1gdZtnkbEsL1jkbnosZh3pHi9tZiwMLmi6tcwjmRQMhz" {
+            println!("This token is valid.");
+        }
+        else {
+            println!("Token is invalid");
+        }
+        */
+    }
 
     Ok(())
 }
