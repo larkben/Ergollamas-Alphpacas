34ca8311c56985a79fdb80b317724df45d1ebab5
Benjamin Larkey <60051088+larkben@users.noreply.github.com>
2023-06-29 10:51:17 -0500

    contracts and init

---
+++ alphpacas/alephium.config.ts
@@ -0,0 +1,33 @@
+import { Configuration } from '@alephium/cli'
+import { Number256 } from '@alephium/web3'
+
+// Settings are usually for configuring
+export type Settings = {
+  issueTokenAmount: Number256
+}
+const defaultSettings: Settings = { issueTokenAmount: 100n }
+
+const configuration: Configuration<Settings> = {
+  networks: {
+    devnet: {
+      nodeUrl: 'http://localhost:22973',
+      // here we could configure which address groups to deploy the contract
+      privateKeys: ['a642942e67258589cd2b1822c631506632db5a12aabcf413604e785300d762a5'],
+      settings: defaultSettings
+    },
+
+    testnet: {
+      nodeUrl: process.env.NODE_URL as string,
+      privateKeys: process.env.PRIVATE_KEYS === undefined ? [] : process.env.PRIVATE_KEYS.split(','),
+      settings: defaultSettings
+    },
+
+    mainnet: {
+      nodeUrl: process.env.NODE_URL as string,
+      privateKeys: process.env.PRIVATE_KEYS === undefined ? [] : process.env.PRIVATE_KEYS.split(','),
+      settings: defaultSettings
+    }
+  }
+}
+
+export default configuration
