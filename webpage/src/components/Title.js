f579fa07641b5d8fcc75dc040ca948d6042aca6a
Benjamin Larkey <60051088+larkben@users.noreply.github.com>
2023-07-05 08:03:55 -0500

    title page and small extras

---
+++ webpage/src/components/Title.js
@@ -0,0 +1,26 @@
+// The title for the webpage
+import '../styles/Title.css'
+
+function Title() {
+    return (
+        <div>
+            <div>
+                <div className='Text'>
+                    <h1 id='alphpaca'> ALPHpaca's </h1>
+                    <p> A project built on Alephium </p>
+                </div>
+                <div className='Text'>
+                    <h1> This site is still in development... </h1>
+                </div>
+                <div className='Text'>
+                    <h2> Stop Scrolling! </h2>
+                </div>
+                <div className='Text'>
+                    <h2> Let Me Cook! </h2>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Title
--- webpage/src/index.js
+++ webpage/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
-import App from './components/App';
+
+import Title from './components/Title'; // ? Error here but the code still works fine
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Title />
   </React.StrictMode>
 );
 
