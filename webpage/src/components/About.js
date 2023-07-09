c81871270c398da384795f6cf67bb75afff9adce
Benjamin Larkey <60051088+larkben@users.noreply.github.com>
2023-07-08 22:50:26 -0500

    who is the team
    - still need new ALPHpaca photo for team

--- webpage/src/components/About.js
+++ webpage/src/components/About.js
@@ -2,13 +2,15 @@
 import '../styles/About.css'
 import aboutLogo from '../assets/about.png'
 
+// Will need a new photo for the team section.
+
 function About() {
     return (
         <div>
             <div className='Text'>
                 <h1 style={{paddingBottom: 20, fontSize: 40}}> What is an ALPHpaca? </h1>
                 <div className='container'>
-                    <div className='Description text' style={{float: 'left'}}>
+                    <div className='Description text'>
                         <p>
                             ALPHpaca's are a sparkling water fueled project designed towards bringing open source utilties and Alephium compatible Dapps.
                             There will be a maximum supply of 1152 ALPHpacas.
@@ -19,7 +21,26 @@ function About() {
                         </p>
                     </div>
                     <div className='image'>
-                        <img src={aboutLogo} alt='ALPHpaca Example' style={{float: 'right'}}/>
+                        <img src={aboutLogo} alt='ALPHpaca Example'/>
+                    </div>
+                </div>
+            </div>
+            <div className='Text'>
+                <h1 style={{paddingBottom: 20, fontSize: 40}}> Who is the Team? </h1>
+                <div className='container'>
+                    <div className='image'>
+                        <img src={aboutLogo} alt='ALPHpaca Example'/>
+                    </div>
+                    <div className='Description text'>
+                        <p>
+                            The ALPHpaca team as of now is myself "Benjamin" and 3 secretly chosen and trusted Alephium members to deliver quality control
+                            or in other words the best ALPHpaca designs.
+                            <br/> <br/>
+                            My Credentials:
+                            I am a senior college student studying Software Development with as of now a minor in Finance.
+                            I am fluent in most programming structures and syntax; I am mostly a fan of Rust and Typescript.
+
+                        </p>
                     </div>
                 </div>
             </div>
