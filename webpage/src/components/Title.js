2e7581c7f90501961d8b023eeaed4265c2ccb5f9
Benjamin Larkey <60051088+larkben@users.noreply.github.com>
2023-07-11 18:32:56 -0500

    required edits

--- webpage/src/components/Title.js
+++ webpage/src/components/Title.js
@@ -8,9 +8,6 @@ function Title() {
                 <div className='Text' >
                     <h1 id='alphpaca'> ALPHpaca's </h1>
                     <p> A project built on Alephium </p>
-                    <div class="Text" style={{minHeight: 0}}>
-                        <a href="#section1" class="arrow animated bounce"> </a>
-                    </div>
                 </div>
             </div>
         </div>
